7f9ae9a4c94aa3a16a85b67e6ccb0df9677fd4d9
Gabriel Piveta <gpiveta@proton.me>
2026-06-11 20:19:52 -0300

    Make it compile with MSVC

--- src/buffer.h
+++ src/buffer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <optional>
 #include <vk_mem_alloc.hpp>
 #include <vulkan/vulkan.hpp>
 #include <vk_mem_alloc.hpp>
--- src/device.h
+++ src/device.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <optional>
 #include <memory>
 #include <vulkan/vulkan.hpp>
 #include <vk_mem_alloc.hpp>
